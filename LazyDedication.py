4af5f7884a61d04fb7f8d35457094835e3f0287e
Sven Friedrich <sven.friedrich@tu-clausthal.de>
2019-05-09 11:49:49 +0900

    Ignores Whitespaces now

--- LazyDedication.py
+++ LazyDedication.py
@@ -1,7 +1,7 @@
 import sys
 
 text = ' '.join(sys.argv[1:])
-for i in range(0, text.__len__() + 1):
-    print(text[0:i])
-for i in range(text.__len__() - 1, 0, -1):
-    print(text[0:i])
+for i in range(0, text.__len__()):
+    if text[i - 1] is not ' ': print(text[0:i])
+for i in range(text.__len__(), 0, -1):
+    if text[i - 1] is not ' ': print(text[0:i])
